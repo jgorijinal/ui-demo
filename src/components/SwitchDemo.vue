4d20f5465fa1b1ad159cfd01fc8a56c1aeea88bd
jgoriginal <932274204@qq.com>
2021-12-25 20:38:39 +0800

    完成switch组组件的css和动画

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,45 +1,13 @@
 <template>
   <div>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
-  <p>这是switch文档</p>
+  <Switch />
   </div>
 </template>
-<script  lang="ts">
-export default {
 
+<script  lang="ts">
+import Switch from '../lib/Switch.vue'
+export default {
+components:{Switch}
 }
 </script>
 <style>
---
+++ src/lib/Switch.vue
@@ -0,0 +1,52 @@
+<template>
+  <button :class="{checked:x}" @click="toggle">
+    <span></span>
+  </button>
+</template>
+
+<script>
+import {ref} from "vue";
+
+export default {
+  name: "Switch",
+  setup(){
+    const x = ref(false)
+    const toggle = ()=>{
+      x.value= !x.value
+    }
+    return{x, toggle}
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+$h:22px;
+$h2: $h - 4px;
+button {
+  width:$h *2;
+  height: $h;
+  background:grey;
+  border: none;
+  border-radius: $h/2;
+  position: relative;
+}
+span{
+  display: inline-block;
+  width:$h2;
+  height:$h2;
+  background: white;
+  border-radius: $h/2;
+  position: absolute;
+  left:2px;
+  top:2px;
+  transition: left 250ms ;
+}
+button.checked{
+  background: blue;
+ span{
+   left:23px;
+ }
+}
+
+</style>
